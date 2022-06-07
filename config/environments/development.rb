d277bcc638933ab4ce198b5357e0cbb1d1874461
stefan-danila-golfgenius <106440100+stefan-danila-golfgenius@users.noreply.github.com>
2022-06-07 12:27:15 +0300

    Add password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   
-  host = 'localhost:3000/' # Don't use this literally; use your local dev host instead # Use this on the cloud IDE.
+  host = 'localhost:3000' # Don't use this literally; use your local dev host instead # Use this on the cloud IDE.
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
 
